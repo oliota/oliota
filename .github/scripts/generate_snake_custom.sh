f62e34ea5b26fa90a805b40a5f5ed85760e0641e
Rubem Oliota <rubemoliota@gmail.com>
2025-08-01 16:44:11 +0100

    Update generate_snake_custom.sh

--- .github/scripts/generate_snake_custom.sh
+++ .github/scripts/generate_snake_custom.sh
@@ -33,7 +33,6 @@ done
 LABELS=""
 declare -A YEAR_RANGES=()
 
-# Meses no eixo Y=0
 for i in "${!MONTHS[@]}"; do
   NAME=${MONTHS[$i]}
   YEAR=${YEARS[$i]}
@@ -53,18 +52,16 @@ for i in "${!MONTHS[@]}"; do
   fi
 done
 
-# Gera linhas horizontais e textos para os anos
 for YEAR in "${!YEAR_RANGES[@]}"; do
   IFS=':' read -r START END <<< "${YEAR_RANGES[$YEAR]}"
   X1=$(awk "BEGIN { printf \"%d\", $PADDING + ($USABLE_WIDTH * $START / 13) }")
   X2=$(awk "BEGIN { printf \"%d\", $PADDING + ($USABLE_WIDTH * (($END + 1) / 13)) }")
-  MID_X=$(awk "BEGIN { printf \"%d\", ($X1 + $X2) / 2 }")
+  MID_X=$(awk "BEGIN { printf \"%d\", ($X1 + $X2)/2 }")
 
   LABELS="${LABELS}<line x1=\"$X1\" y1=\"-15\" x2=\"$X2\" y2=\"-15\" stroke=\"#000\" stroke-width=\"2\" />\n"
   LABELS="${LABELS}<text x=\"$MID_X\" y=\"-20\" font-size=\"14\" fill=\"#000\" text-anchor=\"middle\">$YEAR</text>\n"
 done
 
-# Gera linhas verticais divisórias entre os anos, exceto após o último ano
 YEARS_SORTED=($(echo "${!YEAR_RANGES[@]}" | tr ' ' '\n' | sort))
 for ((i=0; i < ${#YEARS_SORTED[@]} - 1; i++)); do
   CUR_YEAR=${YEARS_SORTED[$i]}
