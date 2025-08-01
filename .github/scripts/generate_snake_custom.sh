e774cafb7f5cde193cd03b6f54d29dbedf666655
Rubem Oliota <rubemoliota@gmail.com>
2025-08-01 15:53:40 +0100

    Update generate_snake_custom.sh

--- .github/scripts/generate_snake_custom.sh
+++ .github/scripts/generate_snake_custom.sh
@@ -2,13 +2,10 @@
 set -e
 
 ORIGINAL="dist/github-contribution-grid-snake.svg"
-TARGET="img/snake/snake_updated.svg"
+TARGET="img/svg/snake/snake_updated.svg"
 
-mkdir -p img/snake
+mkdir -p img/svg/snake
 
 BORDER='<rect x="-16" y="-32" width="880" height="192" rx="20" ry="20" fill="none" stroke="#999" stroke-width="2"/>'
 
 echo "$(cat "$ORIGINAL")" | sed "s|<svg[^>]*>|&\n$BORDER|" > "$TARGET"
-
-mkdir -p dist/img/snake
-cp "$TARGET" dist/img/snake/snake_updated.svg
